8f12bab0fd4bc0d1e4ef1d3666891d89ce1c7fae
Joshua Howard <Josh@knowthefacts.com>
2020-10-06 06:41:47 -0500

    Spider: AlphaAdvantage POC Spider

--- stockspiders/stockspiders/spiders/alphavantage.py
+++ stockspiders/stockspiders/spiders/alphavantage.py
@@ -5,8 +5,14 @@ import json
 import os
 import scrapy
 import numpy as np
-#import tensorflow as tf
+import tensorflow as tf
 from sklearn.preprocessing import MinMaxScaler
+import math
+import logging
+
+
+def roundDown(x):
+    return int(math.floor(x / 1000.0)) * 1000
 
 class AlphaVantage(scrapy.Spider):
     name = "alphavantage"
@@ -15,6 +21,8 @@ class AlphaVantage(scrapy.Spider):
         ticker = getattr(self, 'ticker', None)
         if (ticker is None):
             raise ValueError('Please provide a ticker symbol!')
+
+        logging.getLogger('matplotlib').setLevel(logging.WARNING)
 
         apikey = os.getenv('alphavantage_apikey')
         print(apikey)
@@ -55,38 +63,43 @@ class AlphaVantage(scrapy.Spider):
         #plt.show()
 
         print('Build Data Sets')
+        #todo: build closing price range
         high_prices = df.loc[:, 'High'].to_numpy()
         low_prices = df.loc[:, 'Low'].to_numpy()
         mid_prices = (high_prices+low_prices)/2.0
 
-        train_data = mid_prices[:11000]
-        test_data = mid_prices[11000:]
+        totalDataPoints = len(mid_prices)
+        trainNum = round(totalDataPoints*.714)
+        testNum = round(totalDataPoints*.286)
+        train_data = mid_prices[:trainNum]
+        test_data = mid_prices[trainNum:]
 
         scaler = MinMaxScaler()
         train_data = train_data.reshape(-1, 1)
         test_data = test_data.reshape(-1, 1)
 
         print('Train the Scaler with training data and smooth data')
-        smooting_window_size = 2500
-        for di in range(0, 10000, smooting_window_size):
-            scaler.fit(train_data[di:di+smooting_window_size,:])
-            train_data[di:di+smooting_window_size,:] = scaler.transform(train_data[di:di+smooting_window_size,:])
+        smoothing_window_size = 500
+        roundVal = roundDown(trainNum)#todo write round down function
+        for di in range(0, roundVal, smoothing_window_size):
+            scaler.fit(train_data[di:di + smoothing_window_size, :])
+            train_data[di:di + smoothing_window_size, :] = scaler.transform(train_data[di:di + smoothing_window_size, :])
 
         print('You normalize the last bit of remaining data')
-        scaler.fit(train_data[di+smooting_window_size,:])
-        train_data[di+smooting_window_size:,:] = scaler.transform(train_data[di+smooting_window_size:,:])
+        scaler.fit(train_data[di + smoothing_window_size:, :])
+        train_data[di + smoothing_window_size:, :] = scaler.transform(train_data[di + smoothing_window_size:, :])
 
-        #reshape both train and test data
+        print("reshape both train and test data")
         train_data = train_data.reshape(-1)
 
-        #normalize test data
+        print("normalize test data")
         test_data = scaler.transform(test_data).reshape(-1)
 
         # Now perform exponential moving average smoothing
         # So the data will have a smoother curve than the original ragged data
         EMA = 0.0
         gamma = 0.1
-        for ti in range(11000):
+        for ti in range(trainNum): #changed 11000 to 800
             EMA = gamma*train_data[ti] + (1-gamma)*EMA
             train_data[ti] = EMA
 
@@ -119,3 +132,35 @@ class AlphaVantage(scrapy.Spider):
         plt.ylabel('Mid Price')
         plt.legend(fontsize=18)
         plt.show()
+
+        window_size = 100
+        N = train_data.size
+
+        run_avg_predictions = []
+        run_avg_x = []
+
+        mse_errors = []
+
+        running_mean = 0.0
+        run_avg_predictions.append(running_mean)
+
+        decay = 0.5
+
+        for pred_idx in range(1, N):
+            running_mean = running_mean * decay + (1.0 - decay) * train_data[pred_idx - 1]
+            run_avg_predictions.append(running_mean)
+            mse_errors.append((run_avg_predictions[-1] - train_data[pred_idx]) ** 2)
+            run_avg_x.append(date)
+
+        print('MSE error for EMA averaging: %.5f' % (0.5 * np.mean(mse_errors)))
+
+        plt.figure(figsize=(18, 9))
+        plt.plot(range(df.shape[0]), all_mid_data, color='b', label='True')
+        plt.plot(range(0, N), run_avg_predictions, color='orange', label='Prediction')
+        # plt.xticks(range(0,df.shape[0],50),df['Date'].loc[::50],rotation=45)
+        plt.xlabel('Date')
+        plt.ylabel('Mid Price')
+        plt.legend(fontsize=18)
+        plt.show()
+
+        print('done')
